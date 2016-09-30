76bc41c1a8e014a6121746fb1f1d153cf4cebfdd
Vladimir Vasic <vladimir@vasic.us>
2016-09-30 16:31:17 +0200

    Update freepbx-install-2.sh

--- freepbx-install-2.sh
+++ freepbx-install-2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 yum -y update
 yum -y groupinstall core base "Development Tools"
-yum install gcc gcc-c++ lynx bison mysql-devel mysql-server php php-mysql php-pear php-mbstring php-xml tftp-server httpd make ncurses-devel libtermcap-devel sendmail sendmail-cf caching-nameserver sox newt-devel libxml2-devel libtiff-devel audiofile-devel gtk2-devel subversion kernel-devel git subversion kernel-devel php-process crontabs cronie cronie-anacron wget vim php-xml uuid-devel libtool sqlite-devel unixODBC mysql-connector-odbc libuuid-devel binutils-devel php-ldap
+yum -y install gcc gcc-c++ lynx bison mysql-devel mysql-server php php-mysql php-pear php-mbstring php-xml tftp-server httpd make ncurses-devel libtermcap-devel sendmail sendmail-cf caching-nameserver sox newt-devel libxml2-devel libtiff-devel audiofile-devel gtk2-devel subversion kernel-devel git subversion kernel-devel php-process crontabs cronie cronie-anacron wget vim php-xml uuid-devel libtool sqlite-devel unixODBC mysql-connector-odbc libuuid-devel binutils-devel php-ldap
+wait ${!}
 chkconfig --level 0123456 iptables off
 service iptables stop
 chkconfig --level 345 mysqld on
